8987ca2936e452069196c26385110577ed78df98
周诚 <zhoucheng133@outlook.com>
2024-09-10 21:11:42 +0800

    完成获取日志

--- src/stores/log.ts
+++ src/stores/log.ts
@@ -1,5 +1,9 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { baseURL } from "./network";
+import axios from "axios";
+import token from "./token";
+import { message } from "ant-design-vue";
 
 interface log{
   ok: boolean,
@@ -12,7 +16,17 @@ export default defineStore("log", ()=>{
 
   let logContent=ref<log[]>([]);
 
-  const showLog=()=>{
+  const showLog=async ()=>{
+    const response=(await axios.get(`${baseURL}/api/dl/log`, {
+      headers: {
+        token: token().token
+      }
+    })).data;
+    if(response.ok){
+      logContent.value=response.msg.reverse();
+    }else{
+      message.error("获取日志失败: "+response.msg);
+    }
     showLogDialog.value=true;
   }
 
