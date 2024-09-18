61eb0c00365ed37274917aa506f7b629ba3f9411
周诚 <zhoucheng133@outlook.com>
2024-09-18 18:13:28 +0800

    添加更新列表提示

--- src/pages/bangumi/BangumiView.vue
+++ src/pages/bangumi/BangumiView.vue
@@ -4,37 +4,37 @@
     <a-collapse v-model:activeKey="activeKey">
       <a-collapse-panel key="0" header="星期日">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[0]" :key="index" @click="addHandler(item, 0)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[0]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 0)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="1" header="星期一">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[1]" :key="index" @click="addHandler(item, 1)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[1]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 1)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="2" header="星期二">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[2]" :key="index" @click="addHandler(item, 2)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[2]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 2)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="3" header="星期三">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[3]" :key="index" @click="addHandler(item, 3)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[3]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 3)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="4" header="星期四">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[4]" :key="index" @click="addHandler(item, 4)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[4]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 4)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="5" header="星期五">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[5]" :key="index" @click="addHandler(item, 5)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[5]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 5)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
       <a-collapse-panel key="6" header="星期六">
         <a-flex wrap="wrap" gap="middle">
-          <a-tag class="item" v-for="(item, index) in list[6]" :key="index" @click="addHandler(item, 6)">{{ item }}</a-tag>
+          <a-tag class="item" v-for="(item, index) in list[6]" :key="index" :color="follow(item) ? 'blue' : ''" @click="addHandler(item, 6)">{{ item }}</a-tag>
         </a-flex>
       </a-collapse-panel>
     </a-collapse>
@@ -76,19 +76,32 @@
 import Header from '@/components/Header.vue';
 import InnerLoadingView from '../loading/InnerLoadingView.vue';
 import { onMounted, ref } from 'vue';
-// import { testData } from '@/test/bangumi';
+import { testData } from '@/test/bangumi';
 import token from '@/stores/token';
 import adder from '@/stores/adder';
 import axios from 'axios';
 import { baseURL } from '@/stores/network';
 import { message } from 'ant-design-vue';
+import ls from '@/stores/list';
 
 document.title="AnimeHelper | 每日更新";
 let list=ref<string[][]>([]);
 
+const follow=(title: string)=>{
+  const index=ls().dataSource.find((item)=>item.title==title)
+  if(index){
+    return true;
+  }
+  return false;
+}
+
 const activeKey=ref([0, 1, 2, 3, 4, 5, 6]);
 let loading=ref(true);
 const addHandler=(title: string, weekday: number)=>{
+  if(follow(title)){
+    message.error("列表中已存在");
+    return;
+  }
   adder().showAdd=true;
   adder().add_title=title;
   adder().add_weekday=weekday;
@@ -108,19 +121,23 @@ export interface BangumiItem{
 onMounted(async ()=>{
   token().getToken();
   // 下面是正式内容
-  const response=(await axios.get(`${baseURL}/api/calendar`, {
-    headers: {
-      token: localStorage.getItem("token")
-    }
-  })).data
-  if(response.ok){
-    list.value=response.msg;
-  }else{
-    message.error("请求失败")
-  }
+  // const response=(await axios.get(`${baseURL}/api/calendar`, {
+  //   headers: {
+  //     token: localStorage.getItem("token")
+  //   }
+  // })).data
+  // if(response.ok){
+  //   list.value=response.msg;
+  // }else{
+  //   message.error("请求失败")
+  // }
   // 下面这行是测试用代码
-  // list.value=testData;
+  list.value=testData;
   // END
+
+  if(ls().dataSource.length==0){
+    await ls().getList();
+  }
 
   loading.value=false;
   
