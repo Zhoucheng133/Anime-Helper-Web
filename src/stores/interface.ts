c7d7e121812dfa51906eb5ad46924b6c79b9edef
周诚 <zhoucheng133@outlook.com>
2024-09-29 18:11:17 +0800

    清空

--- src/stores/interface.ts
+++
@@ -1,7 +0,0 @@
-export interface BangumiItem{
-  id: string,
-  title: string,
-  episode: number,
-  now: number,
-  time: number,
-}
--- src/stores/variables.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-
-export default defineStore("mobile", ()=>{
-  let isMobile=ref(false);
-
-  return {isMobile};
-})
