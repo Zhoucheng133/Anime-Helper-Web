88f4507ff148aebbd7357c3e664643125457fd0a
周诚 <zhoucheng133@outlook.com>
2024-09-30 19:42:45 +0800

    完成列表展示

--- hooks/list.ts
+++ hooks/list.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ssrHost } from "./network"
 
-export interface bangumiItem{
+export interface BangumiItem{
   id: string,
   title: string,
   episode: number,
@@ -9,20 +9,114 @@ export interface bangumiItem{
   time: number
 }
 
-export async function getList(): Promise<bangumiItem[]>{
+// 判定是否实际上显示多少集
+export function analyseEpisode(item: BangumiItem){
+  if(item.time==0){
+    return item.episode;
+  }
+  return calculateEpisodesReleased(item.time)>item.episode?item.episode:calculateEpisodesReleased(item.time);
+}
+
+// 计算截至到今天的已更新集数
+export function calculateEpisodesReleased(firstEpisodeTimestamp: number): number {
+  const tmp = new Date();
+  const currentDate = new Date(tmp.getFullYear(), tmp.getMonth(), tmp.getDate());
+  const difference = currentDate.getTime() - firstEpisodeTimestamp;
+  const daysPassed=Math.floor(difference / (1000 * 60 * 60 * 24));
+  const weeksPassed = Math.floor(daysPassed / 7);
+  return Math.max(weeksPassed, 0) + 1;
+}
+
+// 获取列表
+export async function getList(): Promise<BangumiItem[]>{
   const token=useCookie('token');
   if(!token){
     return [];
   }
   const {data: response}=await useAsyncData(async ()=>{
-    return (await axios.get(`${ssrHost}/api/list`, {
+    const data = (await axios.get(`${ssrHost}/api/list`, {
       headers: {
         token: token.value,
       }
     })).data;
+    if(data.ok){
+      return data.msg.reverse();
+    }
+    return [];
   });
-  if(response.value.ok){
-    return response.value.msg;
-  }
-  return [];
+  return response.value;
 }
+
+export const listColumn: any=[
+  {
+    title: '标题',
+    dataIndex: 'title',
+    key: 'title',
+    fixed: 'left',
+    width: 150,
+  },
+  {
+    title: "状态",
+    dataIndex: 'status',
+    key: 'status',
+    width: 70,
+    filters: [
+      {
+        text: '更新中',
+        value: 'onUpdate',
+      },
+      {
+        text: '已完结',
+        value: 'finished'
+      }
+    ],
+    onFilter: (value: string, record: BangumiItem)=>{
+      if(value=="onUpdate"){
+        return calculateEpisodesReleased(record.time)<record.episode;
+      }else{
+        return calculateEpisodesReleased(record.time)>=record.episode
+      }
+    },
+  },
+  {
+    title: "集数",
+    dataIndex: 'episode',
+    key: 'episode',
+    width: 70,
+  },
+  {
+    title: "看至",
+    dataIndex: 'now',
+    key: 'now',
+    width: 70,
+  },
+  {
+    title: "进度",
+    dataIndex: 'progress',
+    key: 'progress',
+    width: 100,
+    filters: [
+      {
+        text: '进行中',
+        value: 'onProgress',
+      },
+      {
+        text: '已看完',
+        value: 'finished'
+      }
+    ],
+    onFilter: (value: string, record: BangumiItem)=>{
+      if(value=="onProgress"){
+        return record.now/analyseEpisode(record)<1;
+      }else{
+        return  record.now/analyseEpisode(record)>=1
+      }
+    },
+  },
+  {
+    title: "操作",
+    dataIndex: 'op',
+    key: 'op',
+    width: 200
+  },
+]
--- pages/list.vue
+++ pages/list.vue
@@ -1,15 +1,76 @@
 <template>
   <PageHeader class="header" :login="true" :page-name="'list'" />
-  <div>
+  <div class="body">
+    <a-table :dataSource="list" :columns="listColumn" :pagination="false" size="small" :scroll="{ x: 500 }" sticky>
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'progress'">
+          <a-progress :percent="record.now/analyseEpisode(record as BangumiItem)*100" :showInfo="false" />
+        </template>
+        <template v-else-if="column.key === 'op'">
+          <a style="user-select: none;" @click="openEdit(record as BangumiItem)">编辑</a>
+          <a style="margin-left: 10px; user-select: none;" @click="addDownloader(record.title)">添加到...</a>
+          <a style="margin-left: 10px; user-select: none;" @click="minus_one(record as BangumiItem)">
+            <i class="bi bi-dash-circle-fill"></i>
+          </a>
+          <a style="margin-left: 10px; user-select: none;" @click="add_one(record as BangumiItem)">
+            <i class="bi bi-plus-circle-fill"></i>
+          </a>
+          <a style="margin-left: 10px; user-select: none;" @click="del_item(record as BangumiItem)">
+            <i class="bi bi-trash3-fill"></i>
+          </a>
+        </template>
+        <template v-if="column.key === 'status'">
+          <a-tag color="green" v-if="calculateEpisodesReleased(record.time)<record.episode" style="user-select: none;">更新中</a-tag>
+          <a-tag v-else style="user-select: none;">已完结</a-tag>
+        </template>
+        <template v-else-if="column.key === 'title'">
+          <div style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">{{ record.title }}</div>
+        </template>
+        <template v-else-if="column.key === 'episode'" >
+          <div style="user-select: none;">
+            {{ analyseEpisode(record as BangumiItem) }}
+          </div>
+        </template>
+        <template v-else-if="column.key === 'now'" >
+          <div style="user-select: none;">
+            {{ record.now }}
+          </div>
+        </template>
+      </template>
+    </a-table>
   </div>
 </template>
 
 <script setup lang="ts">
 import PageHeader from '~/components/PageHeader.vue';
 import init from '~/hooks/init';
-import { getList, type bangumiItem } from '~/hooks/list';
+import { getList, type BangumiItem, listColumn, analyseEpisode, calculateEpisodesReleased } from '~/hooks/list';
 
-let list=ref<bangumiItem[]>([]);
+const del_item=(record: BangumiItem)=>{
+
+}
+
+const add_one=(record: BangumiItem)=>{
+
+}
+
+const minus_one=(record: BangumiItem)=>{
+
+}
+
+const addDownloader=(title: string)=>{
+
+}
+
+const openEdit=(record: BangumiItem)=>{
+
+}
+
+let list=ref<BangumiItem[]>([]);
+let showEdit=ref(false);
+let showAdd=ref(false);
+let showDownloader=ref(false);
+
 useHead({
   title: 'AnimeHelper | 列表'
 })
@@ -20,7 +81,7 @@ if(!islogin){
     window.location.href='/login';
   })
 }else{
-  list.value=(await getList()).reverse();
+  list.value=await getList();
 }
 
 </script>
