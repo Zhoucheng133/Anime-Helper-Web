7946ee359cc9500e7d87daea46d8477835a9219a
周诚 <zhoucheng133@outlook.com>
2024-10-01 20:10:56 +0800

    添加build代码

--- hooks/network.ts
+++ hooks/network.ts
@@ -1,4 +1,5 @@
 export const ssrHost='http://127.0.0.1:8080';
 export const reqHost='http://127.0.0.1:8080';
 // 生产代码 => 
+// export const ssrHost='http://127.0.0.1:5000';
 // export const reqHost='';
