2f87e5ca4956f69d1b055e62e7c0add3852c26a6
周诚 <zhoucheng133@outlook.com>
2024-10-02 21:36:01 +0800

    更新host地址

--- hooks/network.ts
+++ hooks/network.ts
@@ -1,5 +1,6 @@
-export const ssrHost='http://127.0.0.1:8080';
-export const reqHost='http://127.0.0.1:8080';
+// export const ssrHost='http://127.0.0.1:8080';
+// export const reqHost='http://127.0.0.1:8080';
 // 生产代码 => 
-// export const ssrHost='http://127.0.0.1:5000';
-// export const reqHost='';
+// 你需要将ssrHost修改为服务器地址
+export const ssrHost='http://192.168.31.236:5000';
+export const reqHost='';
