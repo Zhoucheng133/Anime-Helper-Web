75c55ba92d7416b30845f90344d66d8d9f04e193
周诚 <zhoucheng133@outlook.com>
2024-10-02 19:53:58 +0800

    完成下载器页面展示

--- hooks/dl.ts
+++ hooks/dl.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { ssrHost } from "./network";
+
 export interface DownloaderItem{
   title: string,
   ass: string
@@ -10,4 +13,70 @@ export interface DownloaderForm{
   exclusions: string[],
   freq: number,
   type: string,
+}
+
+export const exclusionColumn: any=[
+  {
+    label: "值",
+    key: "value",
+  },
+  {
+    label: '操作',
+    key: 'op',
+    rowClass: 'op_style'
+  },
+]
+
+export const bangumiColumn: any=[
+  {
+    label: "字幕组",
+    key: "ass",
+  },
+  {
+    label: "标题",
+    key: "title",
+  },
+  {
+    label: '操作',
+    key: 'op',
+    rowClass: 'op_style'
+  },
+];
+
+export const initFormData=async (): Promise<DownloaderForm | null>=>{
+  const token=useCookie('token');
+  if(!token.value){
+    return null;
+  }
+  const {data: response}=await useAsyncData(async ()=>{
+    const data = (await axios.get(`${ssrHost}/api/dl`, {
+      headers: {
+        token: token.value,
+      }
+    })).data;
+    if(data.ok){
+      return data.msg;
+    }
+    return null;
+  });
+  return response.value;
+}
+
+export const initStatus=async (): Promise<boolean>=>{
+  const token=useCookie('token');
+  if(!token.value){
+    return false;
+  }
+  const {data: response}=await useAsyncData(async ()=>{
+    const data = (await axios.get(`${ssrHost}/api/dl/status`, {
+      headers: {
+        token: token.value,
+      }
+    })).data;
+    if(data.ok){
+      return data.msg;
+    }
+    return false;
+  });
+  return response.value;
 }
--- pages/downloader.vue
+++ pages/downloader.vue
@@ -1,22 +1,98 @@
 <template>
   <PageHeader class="header" :login="true" :page-name="'downloader'" />
-  <div>
+  <div class="body">
+    <div class="dl_item">
+      <div>运行状态</div>
+      <div class="dl_item_content">
+        <UBadge :color="running ? 'green' : 'yellow'" style="margin-right: 20px;">{{ running ? '运行中' : "等待中" }}</UBadge>
+        <UToggle v-model="running" @change="toggleRun" />
+      </div>
+    </div>
+    <div class="dl_item">
+      <div>系统操作</div>
+      <div class="dl_item_content">
+        <UButton size="xs" variant="soft">显示日志</UButton>
+        <UButton size="xs" variant="soft" style="margin-left: 20px;">保存表单</UButton>
+      </div>
+    </div>
+    <div class="dl_item">
+      <div>RSS 来源</div>
+      <div class="dl_item_content">
+        <USelectMenu :options="rssOptions" v-model="formData.type" :disabled='running'>
+          <template #label>
+            {{ formData.type=='mikan' ? 'Mikan' : 'Acgrip' }}
+          </template>
+          <template #option="{ option: item }">
+            {{ item=='mikan' ? 'Mikan' : 'Acgrip' }}
+          </template>
+        </USelectMenu>
+      </div>
+    </div>
+    <div class="dl_item">
+      <div>更新频率</div>
+      <div class="dl_item_content">
+        <div style="width: 100px;"><UInput v-model="formData.freq" type="number"></UInput></div>
+        <div style="margin-left: 10px;">分钟</div>
+      </div>
+    </div>
+    <div class="dl_item">
+      <div>Aria2 地址</div>
+      <div class="dl_item_content">
+        <div style="width: 100%;"><UInput v-model="formData.ariaLink" placeholder="http(s)://" :disabled='running' /></div>
+      </div>
+    </div>
+    <div class="dl_item">
+      <div>Aria2 密钥</div>
+      <div class="dl_item_content">
+        <div style="width: 100%;"><UInput v-model="formData.ariaSecret" placeholder="" type="password" :disabled='running' /></div>
+      </div>
+    </div>
+    <a-collapse style="margin-top: 20px;" v-model:activeKey="showFold">
+      <a-collapse-panel key="1" header="番剧表">
+        <UButton :disabled='running' variant="soft">添加</UButton>
+        <UTable :rows="formData.bangumi" :empty-state="{ icon: 'i-heroicons-circle-stack-20-solid', label: '没有数据' }" :columns="bangumiColumn">
+          <template #op-data="{ row }">
+            <div><UButton size="xs" color="red" variant="soft">删除</UButton></div>
+          </template>
+        </UTable>
+      </a-collapse-panel>
+      <a-collapse-panel key="2" header="排除关键字">
+        <UButton :disabled='running' variant="soft">添加</UButton>
+        <UTable :rows="exclusionRow" :columns="exclusionColumn" >
+          <template #op-data="{ row }">
+            <div><UButton size="xs" color="red" variant="soft">删除</UButton></div>
+          </template>
+        </UTable>
+      </a-collapse-panel>
+    </a-collapse>
   </div>
 </template>
 
 <script setup lang="ts">
 import PageHeader from '~/components/PageHeader.vue';
-import type { DownloaderForm } from '~/hooks/dl';
+import { type DownloaderForm, bangumiColumn, exclusionColumn, initFormData, initStatus } from '~/hooks/dl';
 import init from '~/hooks/init';
 
-let status=ref(false);
+const rssOptions=['mikan', 'acgrip'];
+let inputPort=ref('');
+let showFold=ref(['1', '2']);
+
+const exclusionRow=computed(()=>{
+  return formData.value.exclusions.map((item)=>({value: item}));
+})
+
+const toggleRun=()=>{
+  
+}
+
+let running=ref(false);
 let formData=ref<DownloaderForm>({
   ariaLink: '',
   ariaSecret: '',
   bangumi: [],
   exclusions: [],
-  freq: 0,
-  type: ''
+  freq: 15,
+  type: 'mikan'
 })
 
 useHead({
@@ -29,9 +105,41 @@ if(!islogin){
     window.location.href='/login';
   })
 }else{
-  // followList.value=await initList();
-  // list.value=await initUpdates();
+  const temp=await initFormData();
+  if(temp){
+    formData.value=temp;
+    inputPort.value=temp.freq.toString();
+  }
+  running.value=await initStatus();
 }
 
 
 </script>
+
+<style scoped>
+.dl_item_content{
+  display: flex;
+  align-items: center;
+}
+.dl_item{
+  display: grid;
+  grid-template-columns: 100px auto;
+  height: 35px;
+  align-items: center;
+  margin-top: 10px;
+}
+.body{
+  margin: auto;
+  width: 800px;
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-bottom: 20px;
+  overflow: hidden;
+}
+@media screen and (max-width: 800px) {
+  .body {
+    width: 100%
+  }
+}
+</style>
